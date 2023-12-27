c79fd3f7b354c465694cfb2830a861b9df9529ea
Jason Krein <jasonhalo0@cs.com>
2023-12-26 17:35:08 -0800

    Compile the fix from previous submission, fixing initial load

--- ts_compile/window_management.js
+++ ts_compile/window_management.js
@@ -28,7 +28,9 @@ const updatePlayerSelectionList = function (initialize) {
     if (getPastGames() && initialize) {
         mostRecentGame = [...getPastGames().entries()].reduce((firstElement, secondElement) => secondElement[0] > firstElement[0] ? secondElement : firstElement);
     }
-    else {
+    else if (!initialize) {
+        // Only do this if the page is not being initialized - if this is called when initialize is true (IE: getPastGames() is null) then
+        // we get an error because there's no selected option
         // "mostRecentGame" is a fake game, with players whose names are currently in the selectors
         let teamOnePlayerOne = document.getElementById("team_one_player_one").selectedOptions[0].value;
         let teamOnePlayerTwo = document.getElementById("team_one_player_two").selectedOptions[0].value;
